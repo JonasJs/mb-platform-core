eba3ad48d469e26bcff044adac438fef003de271
Jonas Alves <joonasbalves@gmail.com>
2025-06-12 17:15:29 -0300

    feat: add husky

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { router } from './routes/index.js'
 
 const PORT = process.env.PORT || 3001
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
